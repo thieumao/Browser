29c073c97a07ee7ca1ad9f5cf15010bb2f5af72a
Thieu Mao <thieumao@gmail.com>
2022-02-22 01:50:47 +0700

    [Home Screen] WebView with Pull To Refresh

--- src/containers/Home/index.tsx
+++ src/containers/Home/index.tsx
@@ -1,26 +1,58 @@
-import React, { FC } from 'react';
+import React, { FC, useState, useRef } from 'react';
+import { RefreshControl } from 'react-native';
 import styled from 'styled-components/native';
 import Colors from '@theme/colors';
-import Fonts from '@theme/fonts';
+import { WebView } from 'react-native-webview';
+import { hasNotch } from 'react-native-device-info';
 
 const Container = styled.View`
-  background-color: ${Colors.BLACK};
-  width: 100%;
-  height: 100%;
-  padding-top: 100px;
+  background-color: ${Colors.WHITE};
+  flex: 1;
+  padding-top: ${hasNotch() ? 32 : 0}px;
 `;
 
-const ButtonText = styled(Fonts.Normal)`
-  color: ${Colors.WHITE};
-  text-align: center;
-  font-size: 32px;
+const ScrollView = styled.ScrollView`
+  flex: 1;
+`;
+
+const Content = styled.View`
+  flex: 1;
 `;
 
 const Home: FC = () => {
 
+  const link = 'https://www.google.com.vn/';
+
+  const [refreshing, setRefreshing] = useState(false);
+  const webViewRef = useRef()
+  
+  const onRefresh = React.useCallback(() => {
+    setRefreshing(true);
+    webViewRef?.current?.reload();
+    setTimeout(() => {
+      setRefreshing(false);
+    }, 800);
+  }, [refreshing]);
+
   return (
     <Container>
-      <ButtonText>{'Browser App'}</ButtonText>
+      <ScrollView 
+        contentContainerStyle={{ flex: 1 }}
+        refreshControl={
+          <RefreshControl
+            refreshing={refreshing}
+            onRefresh={onRefresh}
+          />
+      }
+      >
+        <Content>
+          <WebView
+            ref={webViewRef}
+            source={{ uri: link }}
+            style={{ flex: 1 }}
+          />
+        </Content>
+      </ScrollView>
     </Container>
   );
 };
